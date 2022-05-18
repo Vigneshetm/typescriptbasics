c783ec202256389336213c622e3e6b6a94e1bf91
Vignesh <vignesh.etm.22@gmail.com>
2022-05-18 12:31:24 +0530

    saf

--- literaltypets.js
+++ literaltypets.js
@@ -10,4 +10,49 @@ console.log(cus);
 var i1 = document.getElementById('i1');
 // i1.value;
 var i2 = document.getElementById('i2');
-var b1 = document;
+var b1 = document.getElementById('btn1');
+var b2 = document.getElementById('btn2');
+var b3 = document.getElementById('btn3');
+var b4 = document.getElementById('btn4');
+var calculate = function (a, b, cal) {
+    if (cal === 'add') {
+        return a + b;
+    }
+    else if (cal === 'sub') {
+        return a - b;
+    }
+    else if (cal === 'mul') {
+        return a * b;
+    }
+    else if (cal === 'div') {
+        return a / b;
+    }
+};
+b1.addEventListener('click', function () {
+    var v1 = parseInt(i1.value);
+    var v2 = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert("Result(Add)=".concat(calculate(v1, v2, 'add')));
+});
+b2.addEventListener('click', function () {
+    var v1 = parseInt(i1.value);
+    var v2 = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert("Result(Sub)=".concat(calculate(v1, v2, 'sub')));
+});
+b3.addEventListener('click', function () {
+    var v1 = parseInt(i1.value);
+    var v2 = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert("Result(Mul)=".concat(calculate(v1, v2, 'mul')));
+});
+b4.addEventListener('click', function () {
+    var v1 = parseInt(i1.value);
+    var v2 = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert("Result(Div)=".concat(calculate(v1, v2, 'div')));
+});
--- literaltypets.ts
+++ literaltypets.ts
@@ -17,4 +17,54 @@ console.log(cus);
 let i1 = document.getElementById('i1') as HTMLInputElement;
 // i1.value;
 let i2 = document.getElementById('i2') as HTMLInputElement;
-const b1 = document
+const b1 = document.getElementById('btn1') as HTMLButtonElement;
+const b2 = document.getElementById('btn2') as HTMLButtonElement;
+const b3 = document.getElementById('btn3') as HTMLButtonElement;
+const b4 = document.getElementById('btn4') as HTMLButtonElement;
+type cal = 'add'|'sub'|'mul'|'div';
+const calculate = (a:number,b:number,cal : cal) : number=>{
+if(cal === 'add'){
+    return a+b;
+}
+else if (cal === 'sub'){
+    return a-b;
+}else if(cal === 'mul')
+{
+    return a*b;
+}
+else if(cal === 'div'){
+    return a/b;
+}
+}
+
+b1.addEventListener('click',()=>{
+let v1 : number = parseInt(i1.value);
+let v2 : number = parseInt(i2.value);
+i1.value = "";
+i2.value = "";
+alert(`Result(Add)=${calculate(v1,v2,'add')}`);
+});
+
+b2.addEventListener('click',()=>{
+    let v1 : number = parseInt(i1.value);
+    let v2 : number = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert(`Result(Sub)=${calculate(v1,v2,'sub')}`);
+});
+
+b3.addEventListener('click',()=>{
+        let v1 : number = parseInt(i1.value);
+        let v2 : number = parseInt(i2.value);
+        i1.value = "";
+        i2.value = "";
+        alert(`Result(Mul)=${calculate(v1,v2,'mul')}`);
+});
+
+b4.addEventListener('click',()=>{
+    let v1 : number = parseInt(i1.value);
+    let v2 : number = parseInt(i2.value);
+    i1.value = "";
+    i2.value = "";
+    alert(`Result(Div)=${calculate(v1,v2,'div')}`);
+});
